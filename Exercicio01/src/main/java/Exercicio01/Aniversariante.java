84119849dee592908e1aa572e21ce8e84a8569ff
Bernardo-blib <bernardosilvamourthe14@gmail.com>
2026-08-13 21:40:37 -0300

    Adiçao e correçao da pasta Aniversariante

--- Exercicio01/src/main/java/Exercicio01/Aniversariante.java
+++ Exercicio01/src/main/java/Exercicio01/Aniversariante.java
@@ -4,21 +4,41 @@ public class Aniversariante {
 	private String nome;
 	private DataAniversario dataAniversario;
 
-	public Aniversariante (String nome, int dia, int mes) {
+	
+	public Aniversariante(String nome, int dia, int mes) {
 		this.nome = nome;
-		this.dataAniversario = dataAniversario(dia, mes);
+		this.dataAniversario = new DataAniversario(dia, mes);
 	}
-	public Aniversariante (String nome, DataAniversario dataAniversario) {
+
+	
+	public Aniversariante(String nome, DataAniversario dataAniversario) {
 		this.nome = nome;
 		this.dataAniversario = dataAniversario;
 	}
-	//gets e sets...
-	public boolean equalsAniversariante(Object outroAniversariante) {
-		Aniversariante a = outroAniversariante;
-		if (this.nome.equals(a.nome) &&
-				this.dataAniversario.equals(a.dataAniversario))
-		{
-			return true;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public DataAniversario getDataAniversario() {
+		return dataAniversario;
+	}
+
+	public void setDataAniversario(DataAniversario dataAniversario) {
+		this.dataAniversario = dataAniversario;
+	}
+
+	
+	public boolean equals(Object outroObjeto) {
+		if (outroObjeto instanceof Aniversariante) {
+			Aniversariante outroAniversariante = (Aniversariante) outroObjeto;
+			if (this.nome.equals(outroAniversariante.nome) && this.dataAniversario.equals(outroAniversariante.dataAniversario)) {
+				return true;
+			}
 		}
 		return false;
 	}
