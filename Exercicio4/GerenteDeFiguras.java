d1a881f89689e0e6faaab22a5c4e60d856357341
Bernardo-blib <bernardosilvamourthe14@gmail.com>
2026-09-03 11:20:41 -0300

    Update GerenteDeFiguras.java

--- Exercicio4/GerenteDeFiguras.java
+++ Exercicio4/GerenteDeFiguras.java
@@ -1,1 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class GerenteDeFiguras {
+    private List<FiguraGeometrica> figuras;
+
+    public GerenteDeFiguras() {
+        figuras = new ArrayList<FiguraGeometrica>();
+    }
+
+    public void adicionaFigura(FiguraGeometrica fig) {
+        this.figuras.add(fig);
+    }
+
+    public void imprimeFiguras() {
+        for (FiguraGeometrica fig : this.figuras) {
+            System.out.println(fig.getNomeFigura());
+        }
+    }
+
+    public double calculaAreaTotalDeFiguras() {
+        double total = 0.0;
+        for (FiguraGeometrica fig : this.figuras) {
+            total += fig.calculaArea();
+        }
+        return total;
+    }
+
+    public double getMaiorAreaDeFigura() {
+        double maiorArea = 0.0;
+        for (FiguraGeometrica fig : this.figuras) {
+            double area = fig.calculaArea();
+            if (area > maiorArea) {
+                maiorArea = area;
+            }
+        }
+        return maiorArea;
+    }
+
+    public List<FiguraGeometrica> getFiguras() {
+        return this.figuras;
+    }
+}
